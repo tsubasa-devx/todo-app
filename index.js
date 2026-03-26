da8314294b9966a895501954033d40e28689ceeb
tsubasa <otake.tsubasa1005@gmail.com>
2026-03-26 17:18:22 +0900

    PORT追加

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ app.delete('/api/todos/:index', (req, res) => {
 });
 
 // サーバー起動
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running`);
 });
